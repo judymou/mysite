cda5f572daa0bc29ed5958a6682c4b993f58d449
Judy Mou <moujudy@gmail.com>
2015-12-27 23:44:10 -0800

    add peronsal

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,7 @@ function App() {
   var me = this;
   App.prototype.init = function() {
     me._initProjects();  
+    me._initPersonal();
   };
 
   App.prototype._initProjects = function() {
@@ -24,6 +25,26 @@ function App() {
     });
     var $workhtml = $(work);
     $('#work .container').append($workhtml);
+  };
+  
+  App.prototype._initPersonal = function() {
+    var personal = '';
+    var count = 1;
+    window.Personal.forEach(function(p) {
+      personal +=
+              '  <div class="4u 12u(mobile)">' +
+              '    <article class="box style2">' +
+              '      <a href="#" class="image featured"><img src="' + p.imgurl + '" alt="" /></a>' +
+              '      <p>' + p.desc + '</p>' +
+              '    </article>' +
+              '  </div>';
+      if (count % 3 == 0) {
+        personal = '<div class="row">' + personal + '</div>';
+      }
+      count++;
+    });
+    var $personalhtml = $(personal);
+    $('#portfolio .container').append($personalhtml);
   };
 }
 $(function() {
