b91bd1ce85e991441f8f65f741dd6abb8810aebb
Judy Mou <moujudy@gmail.com>
2015-12-27 23:45:11 -0800

    add data file

---
+++ public/data/personal.js
@@ -0,0 +1,14 @@
+window.Personal = [
+  {
+    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xft1/v/t1.0-9/11998812_10156009366860591_5040030783375279008_n.jpg?oh=26f4f84a3d8c959ae26098f9b6aad3a0&oe=571F97A3',
+    desc: 'I like climbing because it gives me a goal to work towards and anyone can excel at it with time and practice.',
+  },
+  {
+    imgurl: 'https://lh3.googleusercontent.com/MWTwcUhSQiLM1ETmzYMUUE_awDjw9t-KWneYEess87FguDek-iCoOr0E7KbpGqjoYaZ5vnJmY_abqy3q8wYeEexqtzAjeIWs3W_IyXNrqKm9VkUdJTwwNE8hdQlYBpR-qOuagSmggzDGH9iJ1ySKltnGIYdlA8wmXBQYkoeTxgNafBbnSlpg8YEgLt8lu1EwfD93e0eociAY2RfzsRS1ABeEL17zzl_LlDZls6etd_Dl_HSLx5anP7VvjPbyz-24BUkJtkOROVVxznVCTRXnB2TfPegpfPKl7IL4J6mO8co5_xUbCXEPm-VRPTVw_m9c303jyc_YBPQQqO20giJUvv0NeDLWgybDa5KZfsr2dRpOi0GqeimDbfF3u4b-rjI-kgqDSNyz3wP14Tu-X4NkMPXMdasl-h4jTDge5x7s749Y9TuhRVuS5mUlsXZomUUg_0vT94aA7EfMnmisdfomplAVOz7SUiK_6L3mYmPdeL_lpqZe2UZeDDdKR0skdQFNsgtqCeVMGGvTQrWTp5AFcKlfob9DS1w7ZEKPcPP7WLjW-MIk1bAAOvtPVwsLufBE9mco=w1080-h801-no',
+    desc: 'I love all animals, especially ones that live in the ocean.'
+  },
+  {
+    imgurl: 'https://anasebrahem.files.wordpress.com/2014/07/shutterstock_129992276.jpg',
+    desc: 'I saw shooting stars for the first time on Aug 12, 2015.'
+  },
+]
