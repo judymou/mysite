a92fa354de3740f5c8692787a6032a007062abc7
Judy Mou <moujudy@gmail.com>
2015-12-28 23:46:29 -0800

    update project

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,14 +1,32 @@
 window.Projects = [
   {
     title: 'Chatalyst',
-    desc: 'Taking the pain out of video chat.',
-    imgurl: 'http://i.imgur.com/yyayOHTl.jpg',
-    url: 'http://chatalyst.io',
+    desc: "Is your workday packed with unproductive meetings? Do you want to get some of those hours back? Chatalyst conducts the perfect on-time and on-task remote meeting by enforcing best meeting practices. It's shockingly fun, I promise :).",
+    imgurl: 'http://i.imgur.com/XJNdwgn.png',
+    url: 'http://www.chatalyst.io',
+  },
+  {
+    title: 'Collegelab',
+    desc: "Collegelab helps students navigate the often-confusing and stressful process of college admission. I moved to the U.S. after middle school and was unfamilar with the whole process. I created Collegelab hoping to help students similar to me.",
+    imgurl: 'http://i.imgur.com/dkDvSQn.jpg',
+    url: 'http://www.collegelab.us/en',
   },
   {
     title: 'Harvest',
-    desc: 'Aerial infrared crop monitoring. Disrupt hackathon winner.',
-    imgurl: 'https://i.imgur.com/SJVfCEg.jpg',
-    url: 'http://harvesters.club',
+    desc: 'Harvest is a toolkit that helps farmers understand plant health and spot water waste using infrared aerial photography. We won <a href="http://devpost.com/software/harvest">DisruptSF</a> 2nd runner up and 3 sponsor prizes.',
+    imgurl: 'http://i.imgur.com/eKOdDWb.png',
+    url: 'http://www.harvesters.club',
+  },
+  {
+    title: 'Circl',
+    desc: 'You ask your friends for advice on everything already, why not also ask them for advice on potential dating matches? :P Now you can on Circl, the first dating app that brings your friends into conversation and helps you make better decisions. Winner of Firebase at LAUNCH 2015.',
+    imgurl: 'http://i.imgur.com/lQm8i0s.png',
+    url: 'http://devpost.com/software/circl-dating-app',
+  },
+  {
+    title: 'Space Balloons',
+    desc: 'I have sent 3 balloons to near space the past year (2015). One of them is part of a successful Kickstarter campaign to launch a potato into near-space. I learned how to prepare for a balloon launch, got amazing pictures, and explored central california (where we launched our balloons). This project is part of a bigger (secret) mission!',
+    imgurl: 'http://i.imgur.com/TqS1BOd.png',
+    url: 'https://www.kickstarter.com/projects/5113868/a-potato-powered-flight-to-near-space/description',
   },
 ]
