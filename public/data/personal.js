337b09a63438bf465c337eca1a282fc73358091b
Judy Mou <moujudy@gmail.com>
2015-12-29 11:30:06 -0800

    update pictures

--- public/data/personal.js
+++ public/data/personal.js
@@ -8,7 +8,7 @@ window.Personal = [
     desc: "I love all animals, especially ones that live in the ocean. I'm quite good at making dolphin noises."
   },
   {
-    imgurl: 'http://news.images.itv.com/image/file/740156/stream_img.jpg',
+    imgurl: 'http://i.imgur.com/DYgm9GJ.jpg',
     desc: 'I saw shooting stars for the first time at the Perseids meteor shower on Aug 12, 2015.'
   },
   {
@@ -16,11 +16,11 @@ window.Personal = [
     desc: 'I was born in Chengdu, the capital of Sichuan & hometown of pandas! I grew up with pandas and like my food extra spicy.'
   },
   {
-    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xtf1/v/t1.0-9/11233428_10155469042370591_4833809611745901590_n.jpg?oh=f1542f50c68d77618bd0d463fde83af9&oe=570B72A6',
+    imgurl: 'http://i.imgur.com/GZwFsMD.jpg',
     desc: 'I meditate for 1 hour a day and do yoga three times a week. I eat an average of 1.2 bananas per day and often go on long walks. I also like swimming in rivers.'
   },
   {
-    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11144449_10204700134402935_4665013033251927812_n.jpg?oh=742014da15dfa960609fae117f1cac11&oe=571F21AF',
+    imgurl: 'http://i.imgur.com/ehgIG1F.jpg',
     desc: "I'm a fan of Eddie Huang. This is me visiting his restaurant Baohaus in NYC! I recommend his traveling show \"Huang's World\" to everyone :)."
   },
 ]
