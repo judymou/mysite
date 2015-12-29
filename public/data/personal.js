788bdc7b4951281025f9e1fc3ee0a8d4df48236a
Judy Mou <moujudy@gmail.com>
2015-12-29 00:45:47 -0800

    update peronsal

--- public/data/personal.js
+++ public/data/personal.js
@@ -1,14 +1,26 @@
 window.Personal = [
   {
-    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xft1/v/t1.0-9/11998812_10156009366860591_5040030783375279008_n.jpg?oh=26f4f84a3d8c959ae26098f9b6aad3a0&oe=571F97A3',
+    imgurl: 'http://i.imgur.com/4Wr3WMa.png',
     desc: 'I like climbing because it gives me a goal to work towards and anyone can excel at it with time and practice.',
   },
   {
-    imgurl: 'https://lh3.googleusercontent.com/MWTwcUhSQiLM1ETmzYMUUE_awDjw9t-KWneYEess87FguDek-iCoOr0E7KbpGqjoYaZ5vnJmY_abqy3q8wYeEexqtzAjeIWs3W_IyXNrqKm9VkUdJTwwNE8hdQlYBpR-qOuagSmggzDGH9iJ1ySKltnGIYdlA8wmXBQYkoeTxgNafBbnSlpg8YEgLt8lu1EwfD93e0eociAY2RfzsRS1ABeEL17zzl_LlDZls6etd_Dl_HSLx5anP7VvjPbyz-24BUkJtkOROVVxznVCTRXnB2TfPegpfPKl7IL4J6mO8co5_xUbCXEPm-VRPTVw_m9c303jyc_YBPQQqO20giJUvv0NeDLWgybDa5KZfsr2dRpOi0GqeimDbfF3u4b-rjI-kgqDSNyz3wP14Tu-X4NkMPXMdasl-h4jTDge5x7s749Y9TuhRVuS5mUlsXZomUUg_0vT94aA7EfMnmisdfomplAVOz7SUiK_6L3mYmPdeL_lpqZe2UZeDDdKR0skdQFNsgtqCeVMGGvTQrWTp5AFcKlfob9DS1w7ZEKPcPP7WLjW-MIk1bAAOvtPVwsLufBE9mco=w1080-h801-no',
-    desc: 'I love all animals, especially ones that live in the ocean.'
+    imgurl: 'http://i.imgur.com/ZZpGXaS.png',
+    desc: "I love all animals, especially ones that live in the ocean. I'm quite good at making dolphin noises."
   },
   {
-    imgurl: 'https://anasebrahem.files.wordpress.com/2014/07/shutterstock_129992276.jpg',
-    desc: 'I saw shooting stars for the first time on Aug 12, 2015.'
+    imgurl: 'http://news.images.itv.com/image/file/740156/stream_img.jpg',
+    desc: 'I saw shooting stars for the first time at the Perseids meteor shower on Aug 12, 2015.'
+  },
+  {
+    imgurl: 'http://imgur.com/hjN9r.jpg',
+    desc: 'I was born in Chengdu, the capital of Sichuan & hometown of pandas! I grew up with pandas and like my food extra spicy.'
+  },
+  {
+    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xtf1/v/t1.0-9/11233428_10155469042370591_4833809611745901590_n.jpg?oh=f1542f50c68d77618bd0d463fde83af9&oe=570B72A6',
+    desc: 'I meditate for 1 hour a day and do yoga three times a week. I eat an average of 1.2 bananas per day and often go on long walks. I also like swimming in rivers.'
+  },
+  {
+    imgurl: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/11144449_10204700134402935_4665013033251927812_n.jpg?oh=742014da15dfa960609fae117f1cac11&oe=571F21AF',
+    desc: "I'm a fan of Eddie Huang. This is me visiting his restaurant Baohaus in NYC! I recommend his traveling show \"Huang's World\" to everyone :)."
   },
 ]
